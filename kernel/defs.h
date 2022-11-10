8b3e0db9fb71535365536dd6512005fa553a2edb
Ali-Noroozbeigy <ali.noroozbeigy@gmail.com>
2022-11-10 22:50:20 +0330

    add free space prototype to defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -63,6 +63,7 @@ void            ramdiskrw(struct buf*);
 void*           kalloc(void);
 void            kfree(void *);
 void            kinit(void);
+float           kfreespace(void);
 
 // log.c
 void            initlog(int, struct superblock*);
